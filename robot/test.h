081ac35d46f6791e4be6e43286644d52ad6eb11a
vivininin <vivininin@protonmail.com>
2024-07-30 11:23:39 +0300

    Implement OAuth2

---
+++ robot/test.h
@@ -0,0 +1,9 @@
+#pragma once
+void test1();
+void test2();
+void test3();
+void test4();
+void test5();
+// vf: the forward speed(in m/s)
+// w: the angle-velocity(in rad/s)
+void moveTurn(double vf, double w);
